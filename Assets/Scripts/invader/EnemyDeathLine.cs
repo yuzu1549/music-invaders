94310298e32320d88054c6e4393c3aa933c5871e
yuzu1549 <yuzu11549@gmail.com>
2026-06-27 01:01:05 +0900

    敵の死亡ラインを追加
    EnemyDeathLineを設置し、Enemyが触れた際に対象を破壊し、プレイヤーがダメージを受けるように設計

---
+++ Assets/Scripts/invader/EnemyDeathLine.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemyDeathLine : MonoBehaviour
+{
+    [Header("プレイヤーのゲームオブジェクト")]
+    public GameObject player; // プレイヤーのゲームオブジェクトを参照するための変数
+    private PlayerHealth playerHealth; // プレイヤーの体力を参照するための変数
+
+    private void Start()
+    {
+        if (player != null)
+        {
+            playerHealth = player.GetComponent<PlayerHealth>(); // プレイヤーの体力を取得
+        }
+        else
+        {
+            Debug.LogError("EnemyDeathLine: プレイヤーのゲームオブジェクトが設定されていません。");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth?.TakeDamage(enemyHealth.maxHealth); // 敵の体力を0にする
+            }
+
+            playerHealth?.TakeDamage(1); // プレイヤーの体力を1減らす
+        }
+    }
+}
