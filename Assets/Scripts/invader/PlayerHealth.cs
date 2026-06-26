01db94073697d69e643af80068be13f41add3040
yuzu1549 <yuzu11549@gmail.com>
2026-06-27 01:44:08 +0900

    無敵時間の実装
    プレイヤーがダメージを受けてから1秒間、ダメージを受けないように変更
    無敵の間、点滅するように実装
    ダメージを受けたときにサウンドを再生するように設定
    また、SEごとに音量調整が出来るように、AudioManagerのPlaySEに第二変数を追加。デフォルトが1で、値に対し音が増減します

--- Assets/Scripts/invader/PlayerHealth.cs
+++ Assets/Scripts/invader/PlayerHealth.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
@@ -6,12 +7,21 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     public int maxHealth = 3; // プレイヤーの最大体力
     [Header("プレイヤーの現在の体力")]
     public int currentHealth;   // プレイヤーの現在の体力
+    [Header("プレイヤーの無敵時間")]
+    public int invincibleTime = 1; // 無敵時間（秒）
+    [Header("点滅間隔")]
+    [SerializeField] private float blinkInterval = 0.1f;
+    [Header("ダメージを受けたときの音")]
+    [SerializeField] private AudioClip damageSE; // ダメージを受けたときの音
+    private bool isInvincible = false; // 無敵状態かどうか
     private ObjectPool ownerPool; // 所属するオブジェクトプール
+    private SpriteRenderer spriteRenderer; // スプライトレンダラーの参照
     private bool isDead = false; // 死亡フラグ
 
     private void Awake()
     {
         currentHealth = maxHealth; // 初期化
+        spriteRenderer = GetComponent<SpriteRenderer>(); // スプライトレンダラーの取得
     }
 
     private void Update()
@@ -33,11 +43,42 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     /// <param name="damage">受けるダメージ量</param>
     public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
+        if (!isInvincible)
+        {
+            currentHealth -= damage;
+            AudioManager.Instance.PlaySE(damageSE, 4f); // ダメージ音を再生
+            StartCoroutine(InvincibleCoroutine());
+        }
+
         if (currentHealth <= 0)
         {
             Die();
         }
+    }
+
+    /// <summary>
+    /// 無敵状態のコルーチン
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator InvincibleCoroutine()
+    {
+        isInvincible = true;
+
+        float elapsedTime = 0f; // 経過時間の初期化
+
+        while (elapsedTime < invincibleTime)
+        {
+            spriteRenderer.enabled = false; // スプライトを非表示にする
+            yield return new WaitForSeconds(blinkInterval);
+
+            spriteRenderer.enabled = true; // スプライトを表示する
+            yield return new WaitForSeconds(blinkInterval);
+
+            elapsedTime += blinkInterval * 2f;
+        }
+
+        spriteRenderer.enabled = true;
+        isInvincible = false;
     }
 
     /// <summary>
