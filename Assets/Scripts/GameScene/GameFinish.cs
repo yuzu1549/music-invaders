b1d89c7cfaaac7dcc5345e78349d77c99a101854
yuzu1549 <yuzu11549@gmail.com>
2026-07-20 18:40:33 +0900

    ゲームクリア・オーバーの演出追加とバグ修正
    ・ゲームクリア・オーバーの演出を追加しました。ゲームクリア・オーバーになると背景が暗くなって、対応するSEが流れるようになってます。その後、画面全体が暗転します
    ・GameScene2回目以降のスコア反映が出来ていなかった問題を修正しました

--- Assets/Scripts/GameScene/GameFinish.cs
+++ Assets/Scripts/GameScene/GameFinish.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameFinish : MonoBehaviour
 {
     [Header("ゲームオーバー・ゲームクリア表示用のテキスト")]
     [SerializeField] private TMPro.TextMeshProUGUI gameOverText;
+    [Header("暗転用のパネル")]
+    [SerializeField] private Image darkPanel1;
+    [SerializeField] private Image darkPanel2;
+    [SerializeField] private float fadeDuration = 1.0f;
 
     private NoteManager noteManager;
+    [Header("ゲームオーバー・ゲームクリア時のサウンド")]
+    [SerializeField] private AudioClip gameOverClip; // ゲームオーバー時の AudioClip をインスペクターで設定
+    [SerializeField] private AudioClip gameClearClip; // ゲームクリア時の AudioClip をインスペクターで設定
 
     private void Awake()
     {
@@ -35,7 +43,13 @@ public class GameFinish : MonoBehaviour
         {
             gameOverText.text = "GAME OVER";
             gameOverText.fontSize = 100;
+            gameOverText.color = Color.red;
             gameOverText.gameObject.SetActive(true);
+        }
+
+        if (darkPanel1 != null)
+        {
+            darkPanel1.gameObject.SetActive(true);
         }
 
         // ゲーム全体を停止
@@ -50,6 +64,14 @@ public class GameFinish : MonoBehaviour
         }
 
         StartCoroutine(LoadResultScene());
+        if (gameOverClip != null)
+        {
+            AudioManager.Instance.PlaySE(gameOverClip); // ゲームオーバー時のサウンドを再生
+        }
+        else
+        {
+            Debug.LogWarning("GameOverClip が設定されていません。ゲームオーバー時のサウンドを再生できません。");
+        }
 
         Debug.Log("Game Over");
     }
@@ -70,8 +92,24 @@ public class GameFinish : MonoBehaviour
         {
             gameOverText.text = "GAME CLEAR";
             gameOverText.fontSize = 100;
+            gameOverText.color = Color.yellow;
             gameOverText.gameObject.SetActive(true);
         }
+
+        if (darkPanel1 != null)
+        {
+            darkPanel1.gameObject.SetActive(true);
+        }
+
+        if (gameClearClip != null)
+        {
+            AudioManager.Instance.PlaySE(gameClearClip); // ゲームクリア時のサウンドを再生
+        }
+        else
+        {
+            Debug.LogWarning("GameClearClip が設定されていません。ゲームクリア時のサウンドを再生できません。");
+        }
+
 
         // ゲーム全体を停止
         Time.timeScale = 0f;
@@ -90,9 +128,33 @@ public class GameFinish : MonoBehaviour
 
     private IEnumerator LoadResultScene()
     {
-        yield return new WaitForSecondsRealtime(4f); // 4秒待機（リアルタイムで待機）
-        JudgementManager.Instance.OnJudgement -= GameManager.Instance.CostCount; // 判定結果のイベントからメソッドを解除
-        GameManager.Instance.isJudgementHandlerRegistered = false; // 判定結果のイベント登録フラグをリセット
+        yield return new WaitForSecondsRealtime(3f); // 3.5秒待機（リアルタイムで待機）
+        // 先に徐々に暗くする
+        if (darkPanel2 != null)
+        {
+            darkPanel2.gameObject.SetActive(true);
+
+            Color color = darkPanel2.color;
+            color.a = 0f;
+            darkPanel2.color = color;
+
+            float elapsedTime = 0f;
+
+            while (elapsedTime < fadeDuration)
+            {
+                elapsedTime += Time.unscaledDeltaTime;
+
+                float alpha = Mathf.Clamp01(elapsedTime / fadeDuration);
+
+                color.a = alpha;
+                darkPanel2.color = color;
+
+                yield return null;
+            }
+        }
+
+
+        //yield return new WaitForSecondsRealtime(1f); // 1秒待機（リアルタイムで待機）
         gameOverText.gameObject.SetActive(false); // 結果表示を非表示にする
         Time.timeScale = 1f; // 時間を元に戻す
         SceneManager.LoadScene("ResultScene");
